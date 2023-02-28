317651c285f122569306bd6b3d37100b238a325b
Paul Rivière <paulriviere002@gmail.com>
2023-02-28 11:25:23 +0100

    Ajout page de création de tâche

---
+++ app/src/main/java/iut/project/mytodolist/CreationTache.kt
@@ -0,0 +1,11 @@
+package iut.project.mytodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreationTache : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creation_tache)
+    }
+}
--- app/src/main/java/iut/project/mytodolist/MainActivity.kt
+++ app/src/main/java/iut/project/mytodolist/MainActivity.kt
@@ -1,11 +1,19 @@
 package iut.project.mytodolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnTache = findViewById<Button>(R.id.btnTache)
+        btnTache.setOnClickListener {
+            val intent = Intent(this@MainActivity, CreationTache::class.java)
+            startActivity(intent)
+        }
     }
 }
