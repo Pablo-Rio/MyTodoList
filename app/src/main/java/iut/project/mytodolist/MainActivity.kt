f7ca187ec16a8d9ddd64368d87f3af34478edefd
Paul Rivière <paulriviere002@gmail.com>
2023-03-16 16:36:32 +0100

    Ajout bdd

--- app/src/main/java/iut/project/mytodolist/MainActivity.kt
+++ app/src/main/java/iut/project/mytodolist/MainActivity.kt
@@ -1,19 +1,141 @@
 package iut.project.mytodolist
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import android.content.DialogInterface
+import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+import iut.project.mytodolist.adapter.MyListAdapter
+import iut.project.mytodolist.classes.EmpModelClass
+import iut.project.mytodolist.handler.DatabaseHandler
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnTache = findViewById<Button>(R.id.btnTache)
-        btnTache.setOnClickListener {
-            val intent = Intent(this@MainActivity, CreationTache::class.java)
-            startActivity(intent)
+    }
+    //method for saving records in database
+    fun saveRecord(view: View){
+        val id = findViewById<EditText>(R.id.u_id).text.toString()
+        val name = findViewById<EditText>(R.id.u_name).text.toString()
+        val email = findViewById<EditText>(R.id.u_email).text.toString()
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        if(id.trim()!="" && name.trim()!="" && email.trim()!=""){
+            val status = databaseHandler.addEmployee(
+                EmpModelClass(
+                    Integer.parseInt(id),
+                    name,
+                    email
+                )
+            )
+            if(status > -1){
+                Toast.makeText(applicationContext,"record save",Toast.LENGTH_LONG).show()
+                findViewById<EditText>(R.id.u_id).text.clear()
+                findViewById<EditText>(R.id.u_name).text.clear()
+                findViewById<EditText>(R.id.u_email).text.clear()
+            }
+        }else{
+            Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
         }
+
+    }
+    //method for read records from database in ListView
+    fun viewRecord(view: View){
+        //creating the instance of DatabaseHandler class
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        //calling the viewEmployee method of DatabaseHandler class to read the records
+        val emp: List<EmpModelClass> = databaseHandler.viewEmployee()
+        val empArrayId = Array<String>(emp.size){"0"}
+        val empArrayName = Array<String>(emp.size){"null"}
+        val empArrayEmail = Array<String>(emp.size){"null"}
+        var index = 0
+        for(e in emp){
+            empArrayId[index] = e.userId.toString()
+            empArrayName[index] = e.userName
+            empArrayEmail[index] = e.userEmail
+            index++
+        }
+        //creating custom ArrayAdapter
+        val myListAdapter = MyListAdapter(
+            this,
+            empArrayId,
+            empArrayName,
+            empArrayEmail
+        )
+        findViewById<ListView>(R.id.listView).adapter = myListAdapter
+    }
+    //method for updating records based on user id
+    fun updateRecord(view: View){
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.update_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val edtId = dialogView.findViewById(R.id.updateId) as EditText
+        val edtName = dialogView.findViewById(R.id.updateName) as EditText
+        val edtEmail = dialogView.findViewById(R.id.updateEmail) as EditText
+
+        dialogBuilder.setTitle("Update Record")
+        dialogBuilder.setMessage("Enter data below")
+        dialogBuilder.setPositiveButton("Update", DialogInterface.OnClickListener { _, _ ->
+
+            val updateId = edtId.text.toString()
+            val updateName = edtName.text.toString()
+            val updateEmail = edtEmail.text.toString()
+            //creating the instance of DatabaseHandler class
+            val databaseHandler: DatabaseHandler= DatabaseHandler(this)
+            if(updateId.trim()!="" && updateName.trim()!="" && updateEmail.trim()!=""){
+                //calling the updateEmployee method of DatabaseHandler class to update record
+                val status = databaseHandler.updateEmployee(EmpModelClass(Integer.parseInt(updateId),updateName, updateEmail))
+                if(status > -1){
+                    Toast.makeText(applicationContext,"record update",Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
+            }
+
+        })
+        dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+            //pass
+        })
+        val b = dialogBuilder.create()
+        b.show()
+    }
+    //method for deleting records based on id
+    fun deleteRecord(view: View){
+        //creating AlertDialog for taking user id
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.delete_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val dltId = dialogView.findViewById(R.id.deleteId) as EditText
+        dialogBuilder.setTitle("Delete Record")
+        dialogBuilder.setMessage("Enter id below")
+        dialogBuilder.setPositiveButton("Delete", DialogInterface.OnClickListener { _, _ ->
+
+            val deleteId = dltId.text.toString()
+            //creating the instance of DatabaseHandler class
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+            if(deleteId.trim()!=""){
+                //calling the deleteEmployee method of DatabaseHandler class to delete record
+                val status = databaseHandler.deleteEmployee(EmpModelClass(Integer.parseInt(deleteId),"",""))
+                if(status > -1){
+                    Toast.makeText(applicationContext,"record deleted",Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
+            }
+
+        })
+        dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { _, _ ->
+            //pass
+        })
+        val b = dialogBuilder.create()
+        b.show()
     }
 }
---
+++ app/src/main/java/iut/project/mytodolist/classes/EmpModelClass.kt
@@ -0,0 +1,3 @@
+package iut.project.mytodolist.classes
+
+class EmpModelClass (var userId: Int, val userName:String , val userEmail: String)
